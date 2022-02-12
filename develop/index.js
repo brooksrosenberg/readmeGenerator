1495f69b04e7887462b9ce4aa838f0b211283559
Brooks Rosenberg <bjtr@bu.edu>
2022-02-12 16:15:16 -0500

    upload of basic question format. questions run and display

--- develop/index.js
+++ develop/index.js
@@ -2,50 +2,116 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = ({ projectTitle, description, motivation, tableOC, installation, usage, credits, license, features, tests}) =>
+const generateHTML = ({ projectTitle, description, installation, usage, license, features, contributing, tests, email, username}) =>
 
-    `# ${projectTitle}
+ `# ${projectTitle}
 
-## ${description}
+##  Description:
+${description}
 
-- What was your motivation? ${motivation}
-- Why did you build this project?
-- What problem does it solve?
-- What did you learn?
+## Table of Contents 
 
-## Table of Contents ${tableOC}
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-
-## ${installation}
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
 
 
-## ${usage}
+## Installation 
+to install necessary dependencies, run the following command:
+
+${installation}
 
 
-## ${credits}
+## Usage 
+${usage}
 
 
-## ${license}
+## License
+This project is licensed under the ${license} license.
+
+## Contributing
+${contributing}
 
 
-## ${features}
+## Tests
+To run tests, run the following command;
+${tests}
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at ${username} `
 
 
-## ${tests}`
+// TODO: create an array of questions for user input
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your Github username: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email: '
+    },
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'Enter your project title: '
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a short description of your project: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license: ',
+        choices: ['MIT', 'Red', 'Green']
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be used to install dependecies? ',
+        default: ('npm i')
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests? ',
+        default: ('npm test')
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo? ',
+        default: 'Open to public use. Creator is not responsible for use.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo? '
+    },
 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+])
+.then((answers) => {
+    const htmlPageContent = generateHTML(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+    fs.writeFile('README.md', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README!')
+    );
+  });
 
-// TODO: Create a function to initialize app
-function init() { }
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) { }
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() { }
+
+// // Function call to initialize app
+// init();
